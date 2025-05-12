6b368b89f3cd1e2fa5763715ffafe5cca56d97a0
zr19012-prn335-cicloII-2022 <zunigajosueramirez@gmail.com>
2025-05-11 18:35:45 -0600

    Primer commit

---
+++ demo-zr19012/src/main/java/com/iss/zr19012/demo_zr19012/App.java
@@ -0,0 +1,14 @@
+package com.iss.zr19012.demo_zr19012;
+
+/**
+ * Hello world!
+ */
+public class App {
+	public String Hello() {
+		return "Hello world";
+	}
+    public static void main(String[] args) {
+    	App hw = new App();
+    	System.out.println(hw.Hello());
+    }
+}
---
+++ demo-zr19012/src/test/java/com/iss/zr19012/demo_zr19012/AppTest.java
@@ -0,0 +1,19 @@
+package com.iss.zr19012.demo_zr19012;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
